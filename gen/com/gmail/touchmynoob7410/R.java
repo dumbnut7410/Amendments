f739c7a36dee6fd7b58d2a4836d88673d5277d13
dumbnut7410 <jchainsaw@hotmail.com>
2013-09-18 20:33:09 -0700

    made more ghetto skeleton

--- gen/com/gmail/touchmynoob7410/R.java
+++ gen/com/gmail/touchmynoob7410/R.java
@@ -35,15 +35,19 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int eigth=0x7f030001;
         public static final int eleven=0x7f030002;
-        public static final int fifth=0x7f030003;
-        public static final int first=0x7f030004;
-        public static final int fourth=0x7f030005;
-        public static final int nine=0x7f030006;
-        public static final int second=0x7f030007;
-        public static final int seventh=0x7f030008;
-        public static final int sixth=0x7f030009;
-        public static final int tenth=0x7f03000a;
-        public static final int third=0x7f03000b;
+        public static final int fifteenth=0x7f030003;
+        public static final int fifth=0x7f030004;
+        public static final int first=0x7f030005;
+        public static final int fourteenth=0x7f030006;
+        public static final int fourth=0x7f030007;
+        public static final int nine=0x7f030008;
+        public static final int second=0x7f030009;
+        public static final int seventh=0x7f03000a;
+        public static final int sixth=0x7f03000b;
+        public static final int tenth=0x7f03000c;
+        public static final int third=0x7f03000d;
+        public static final int thirteen=0x7f03000e;
+        public static final int twelve=0x7f03000f;
     }
     public static final class menu {
         public static final int main=0x7f070000;
