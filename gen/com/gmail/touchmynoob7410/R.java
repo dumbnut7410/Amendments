5f7f51f6337883178acf44eaab8589b7c60839c7
dumbnut7410 <jchainsaw@hotmail.com>
2013-09-19 19:50:41 -0700

    Completed ghetto skeleton

--- gen/com/gmail/touchmynoob7410/R.java
+++ gen/com/gmail/touchmynoob7410/R.java
@@ -33,21 +33,33 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int eigth=0x7f030001;
-        public static final int eleven=0x7f030002;
-        public static final int fifteenth=0x7f030003;
-        public static final int fifth=0x7f030004;
-        public static final int first=0x7f030005;
-        public static final int fourteenth=0x7f030006;
-        public static final int fourth=0x7f030007;
-        public static final int nine=0x7f030008;
-        public static final int second=0x7f030009;
-        public static final int seventh=0x7f03000a;
-        public static final int sixth=0x7f03000b;
-        public static final int tenth=0x7f03000c;
-        public static final int third=0x7f03000d;
-        public static final int thirteen=0x7f03000e;
-        public static final int twelve=0x7f03000f;
+        public static final int eighteen=0x7f030001;
+        public static final int eigth=0x7f030002;
+        public static final int eleven=0x7f030003;
+        public static final int fifteenth=0x7f030004;
+        public static final int fifth=0x7f030005;
+        public static final int first=0x7f030006;
+        public static final int fourteenth=0x7f030007;
+        public static final int fourth=0x7f030008;
+        public static final int nine=0x7f030009;
+        public static final int nineteen=0x7f03000a;
+        public static final int second=0x7f03000b;
+        public static final int seventeen=0x7f03000c;
+        public static final int seventh=0x7f03000d;
+        public static final int sixteen=0x7f03000e;
+        public static final int sixth=0x7f03000f;
+        public static final int tenth=0x7f030010;
+        public static final int third=0x7f030011;
+        public static final int thirteen=0x7f030012;
+        public static final int twelve=0x7f030013;
+        public static final int twenty=0x7f030014;
+        public static final int twentyfive=0x7f030015;
+        public static final int twentyfour=0x7f030016;
+        public static final int twentyone=0x7f030017;
+        public static final int twentyseven=0x7f030018;
+        public static final int twentysix=0x7f030019;
+        public static final int twentythree=0x7f03001a;
+        public static final int twentytwo=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/gmail/touchmynoob7410/MainActivity.java
+++ src/com/gmail/touchmynoob7410/MainActivity.java
@@ -1,16 +1,119 @@
 package com.gmail.touchmynoob7410;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	int amendmentNumber;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		switch (amendmentNumber){
+		case 0:
+			setContentView(R.layout.activity_main);
+			break;
+		case 1:
+			setContentView(R.layout.first);
+			break;
+		case 2:
+			setContentView(R.layout.second);
+			break;
+		case 3:
+			setContentView(R.layout.third);
+			break;
+		case 4:
+			setContentView(R.layout.fourth);
+			break;
+		case 5:
+			setContentView(R.layout.fifth);
+			break;
+		case 6:
+			setContentView(R.layout.sixth);
+			break;
+		case 7:
+			setContentView(R.layout.seventh);
+			break;
+		case 8:
+			setContentView(R.layout.eigth);
+			break;
+		case 9:
+			setContentView(R.layout.nine);
+			break;
+		case 10:
+			setContentView(R.layout.tenth);
+			break;
+		case 11:
+			setContentView(R.layout.eleven);
+			break;
+		case 12:
+			setContentView(R.layout.twelve);
+			break;
+		case 13:
+			setContentView(R.layout.thirteen);
+			break;
+		case 14:
+			setContentView(R.layout.fourteenth);
+			break;
+		case 15:
+			setContentView(R.layout.fifteenth);
+			break;
+		case 16:
+			setContentView(R.layout.sixteen);
+			break;
+		case 17:
+			setContentView(R.layout.seventeen);
+			break;
+		case 18:
+			setContentView(R.layout.eighteen);
+			break;
+		case 19:
+			setContentView(R.layout.nineteen);
+			break;
+		case 20:
+			setContentView(R.layout.twenty);
+			break;
+		case 21:
+			setContentView(R.layout.twentyone);
+			break;
+		case 22:
+			setContentView(R.layout.twentytwo);
+			break;
+		case 23:
+			setContentView(R.layout.twentythree);
+			break;
+		case 24:
+			setContentView(R.layout.twentyfour);
+			break;
+		case 25:
+			setContentView(R.layout.twentyfive);
+			break;
+		case 26:
+			setContentView(R.layout.twentysix);
+			break;
+		case 27:
+			setContentView(R.layout.twentyseven);
+			break;
 	}
+		Button submit = (Button) findViewById(R.id.button1);
+		OnClickListener listener = new OnClickListener() {
+					
+				@Override
+				public void onClick(View v) {
+				amendmentNumber++;
+				onCreate(new Bundle());
+			}
+		};
+		
+		submit.setOnClickListener(listener);
+		
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
